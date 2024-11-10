ff9f440607d6b56117cbb6e7807defd301fae65a
Frans Fürst <frans.fuerst+om-office@protonmail.com>
2024-11-10 22:02:07 +0100

    Auto-format using ruff, add ruff to project dependencies

--- example_simple.py
+++ example_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import time
-import numpy as np
-import irpythermal
 
+import numpy as np
+
+import irpythermal
 
 # create camera object and do a initial calibration
 cap = irpythermal.HT301()
@@ -14,26 +15,49 @@ ret, frame = cap.read()
 info, temperature_lookup_table = cap.info()
 
 point = (10, 20)
-print('temperature at point:', point, 'is:', temperature_lookup_table[frame[point]], '°C', '[raw ADC value:', frame[point], ']')
-#print('additional info:', info)
+print(
+    "temperature at point:",
+    point,
+    "is:",
+    temperature_lookup_table[frame[point]],
+    "°C",
+    "[raw ADC value:",
+    frame[point],
+    "]",
+)
+# print('additional info:', info)
 
-time.sleep(5) # not needed
-print('click, click')
+time.sleep(5)  # not needed
+print("click, click")
 cap.calibrate()
-time.sleep(5) # not needed
+time.sleep(5)  # not needed
 
 # read next frame
 ret, frame = cap.read()
 info, temperature_lookup_table = cap.info()
 
-print('temperature at point:', point, 'is:', temperature_lookup_table[frame[point]], '°C', '[raw ADC value:', frame[point], ']')
-#print('additional info:', info)
+print(
+    "temperature at point:",
+    point,
+    "is:",
+    temperature_lookup_table[frame[point]],
+    "°C",
+    "[raw ADC value:",
+    frame[point],
+    "]",
+)
+# print('additional info:', info)
 
-print('temperature at any point:', temperature_lookup_table[frame])
-print('frame shape:', frame.shape, 'data type:', frame.dtype)
-#result: frame shape: (288, 384) data type: uint16
-print('T lut shape:', temperature_lookup_table.shape, 'data type:', temperature_lookup_table.dtype)
-#result: T lut shape: (16384,) data type: float64
+print("temperature at any point:", temperature_lookup_table[frame])
+print("frame shape:", frame.shape, "data type:", frame.dtype)
+# result: frame shape: (288, 384) data type: uint16
+print(
+    "T lut shape:",
+    temperature_lookup_table.shape,
+    "data type:",
+    temperature_lookup_table.dtype,
+)
+# result: T lut shape: (16384,) data type: float64
 
 # release camera object
 cap.release()
